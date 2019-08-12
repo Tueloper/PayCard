3fe098575cbbd2e0174db67c9f09b2e54b63194a
Tueloper <ozurumbatochuku@yahoo.com>
2019-08-12 18:24:32 +0100

    Shopping Cart created

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//variables
+const courses = document.querySelector('#courses-list');
+
+//Event Listeners
+//load the event listeners
+loadListeners();
+
+function loadListeners() {
+    
+    courses.addEventListener('click', buyCourse);
+}
+
+//Function
+function buyCourse(e) {
+    
+}
